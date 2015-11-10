16c40de78cacfb5b6e52964b4763b5ec6801305a
Shahvaiz Hanif <shahvaiz@users.noreply.github.com>
2015-11-09 22:05:48 -0500

    URLs

--- myapp/views.py
+++ myapp/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from django.shortcuts import render_to_response
+from myapp.models import geoname
 
-# Create your views here.
+
+def test(request):
+	#abc = geoname.objects.filter(elevation__gte = '5000')
+	abc = geoname.objects.all()[:5]
+	return render_to_response('test.html', {'abc': abc})
+
--- mysite/urls.py
+++ mysite/urls.py
@@ -15,7 +15,9 @@ Including another URLconf
 """
 from django.conf.urls import include, url
 from django.contrib import admin
+from myapp import views
 
 urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
+    url(r'^test/$', 'myapp.views.test', name='index'),
 ]

b1279bb66e0a2cfb5112ccc555bfef72e869b2cf
Shahvaiz Hanif <shahvaiz@users.noreply.github.com>
2015-11-11 14:45:46 -0500

    settings

--- README.md
+++
@@ -1,2 +0,0 @@
-####Database
-https://us-east1.postgresqlstudio.com/pgstudio/Heroku.jsp?app=murmuring-sea-2303&addon=pgstudio
--- mysite/settings.py
+++ mysite/settings.py
@@ -111,7 +111,7 @@ STATIC_URL = '/static/'
 # Parse database configuration from $DATABASE_URL
 import dj_database_url
 
-DATABASES['default'] =  dj_database_url.config()
+#DATABASES['default'] =  dj_database_url.config()
 
 DATABASES['default'] =  dj_database_url.config(default='postgres://shahvaiz@localhost/traveldb')
 #DATABASES['default']['ENGINE'] = 'django.db.backends.postgresql_psycopg2'
